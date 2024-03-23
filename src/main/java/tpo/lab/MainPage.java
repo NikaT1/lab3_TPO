19391adcf32a704b42fc8bfdc9c9cfc4903dca16
NastyaLush <yfcn.if1203@gmail.com>
2024-03-23 15:06:40 +0300

    main page tests added

--- src/main/java/tpo/lab/MainPage.java
+++ src/main/java/tpo/lab/MainPage.java
@@ -1,12 +1,15 @@
 package tpo.lab;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+import lombok.Getter;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
-public class MainPage extends Page{
+public class MainPage extends Page {
 
 
     public MainPage(WebDriver driver) {
@@ -37,29 +40,220 @@ public class MainPage extends Page{
     private WebElement feedbackButton;
     @FindBy(xpath = "/html/body/div[2]/footer/div/ul/li[7]/a")
     private WebElement oldVersionButton;
+    @FindBy(xpath = "/html/body/div[2]/nav/a")
+    private WebElement fastPicButton;
+    @FindBy(xpath = "/html/body/div[2]/nav/button")
+    private WebElement toolBarButton;
+    @FindBy(xpath = "//*[@id=\"navbarNavAltMarkup\"]/div[1]/a[1]")
+    private WebElement downloadButton;
+    @FindBy(xpath = "//*[@id=\"navbarNavAltMarkup\"]/div[1]/a[2]")
+    private WebElement downloadByLink;
+    @FindBy(xpath = "//*[@id=\"navbarNavAltMarkup\"]/div[2]/a[1]")
+    private WebElement myDownloadsButton;
+    @FindBy(xpath = "//*[@id=\"navbarNavAltMarkup\"]/div[2]/a[2]")
+    private WebElement enterButton;
 
+    @FindBy(xpath = "/html/body/div[2]/main/div/section/div/div[5]/button[2]")
+    private WebElement restrictionsButton;
+    @FindBy(xpath = "/html/body/div[2]/main/div/section/div/div[5]/button[1]")
+    private WebElement settingButton;
+    @FindBy(xpath = "//*[@id=\"select\"]")
+    private WebElement selectImageButton;
+    @FindBy(xpath = "/html/body/input")
+    private WebElement hiddenSelectImageButton;
+    @FindBy(xpath = "/html/body/div[2]/main/div/section/div/div[4]/div/button")
+    private WebElement beginDownloadButton;
+    @Getter
+    private Preview preview = new Preview();
+    @Getter
+    private Image image = new Image();
+    @Getter
+    private DownloadSettings downloadSettings = new DownloadSettings();
 
     public void clickOldVersionButton() {
         oldVersionButton.click();
     }
-    public void clickOnFaqButton(){
+
+    public void clickOnFaqButton() {
         faqbutton.click();
     }
-    public void clickOnRuleButton(){
+
+    public void clickOnRuleButton() {
         ruleseButton.click();
     }
-    public void clickOnUploaderButton(){
+
+    public void clickOnUploaderButton() {
         uploaderButton.click();
     }
-    public void clickOnAndroidAppButton(){
+
+    public void clickOnAndroidAppButton() {
         androidAppButton.click();
     }
-    public void clickOnFeedBackButton(){
+
+    public void clickOnFeedBackButton() {
         feedbackButton.click();
     }
-    public void clickOnAboutServiceButton(){
+
+    public void clickOnAboutServiceButton() {
         aboutServiceButton.click();
     }
 
+    public void clickOnFastPicButton() {
+        fastPicButton.click();
+    }
+
+    public void clickOnToolBarButton() {
+        toolBarButton.click();
+    }
+
+    public void clickOnDownloadButton() {
+        downloadButton.click();
+    }
+
+    public void clickOnDownloadByLinkButton() {
+        downloadByLink.click();
+    }
+
+    public void clickOnMyDownloadsButton() {
+        myDownloadsButton.click();
+    }
+
+    public void clickOnEnterButton() {
+        enterButton.click();
+    }
+
+    public void selectImage(String path) {
+        selectImageButton.click();
+        hiddenSelectImageButton.sendKeys(path);
+    }
+
+    public void clickOnSettingsButton() {
+        settingButton.click();
+    }
+
+    public void clickOnRestrictionsButton() {
+        restrictionsButton.click();
+    }
+
+    public void clickOnBeginDownloadButton() {
+        beginDownloadButton.click();
+    }
+
+    public boolean isFinishedDownloading() {
+        return driver.getCurrentUrl().startsWith(ConfProperties.getProperty("albumPage"));
+    }
+    public class DownloadSettings{
+        public DownloadSettings() {
+            PageFactory.initElements(driver, this);
+        }
+        @FindBy(xpath = "/html/body/div[2]/main/div/section/div/div[2]/div/select")
+        private WebElement deleteSettings;
+        @FindBy(xpath = "/html/body/div[2]/main/div/section/div/div[3]/div/input")
+        private WebElement albumName;
+        @FindBy(xpath = "//*[@id=\"choose-album\"]")
+        private WebElement selectAlbum;
+
+        public void addAlbumName(String text){
+            albumName.sendKeys(text);
+        }
+        public void selectDeleteOptionByIndex(Integer index){
+            new Select(deleteSettings).selectByIndex(index);
+        }
+        public void selectAlbumByIndex(Integer index){
+            new Select(selectAlbum).selectByIndex(index);
+        }
+        public List<WebElement> getMyAlbums(){
+            return new Select(selectAlbum).getOptions();
+        }
+
+    }
+
+    public class Image{
+        public Image() {
+            PageFactory.initElements(driver, this);
+        }
+        @FindBy(xpath = "//*[@id=\"collapseSettings\"]/div[2]/div/div/div/div[1]/label")
+        private WebElement decreaseTillButton;
+        @FindBy(xpath = "//*[@id=\"orig-resize\"]")
+        private WebElement decreaseTillText;
+        @FindBy(xpath = "//*[@id=\"collapseSettings\"]/div[2]/div/div/div/div[2]/label")
+        private WebElement decreaseInBrowser;
+        @FindBy(xpath = "//*[@id=\"collapseSettings\"]/div[2]/div/div/div/div[3]/label")
+        private WebElement optimizeInJPG;
+        @FindBy(xpath = "//*[@id=\"jpeg-quality\"]")
+        private WebElement optimizeInJPGText;
+        @FindBy(xpath = "//*[@id=\"collapseSettings\"]/div[2]/div/div/div/div[4]/label")
+        private WebElement cover;
+
+        public void clickOnDeacreaseTillButton(){
+            decreaseTillButton.click();
+        }
+        public void addDeacreaseText(String text){
+            decreaseTillText.sendKeys(text);
+        }
+        public void addOptimiseInJpgText(String text){
+            optimizeInJPGText.sendKeys(text);
+        }
+        public void clickOnDecreaseInBrowser(){
+            decreaseInBrowser.click();
+        }
+        public void clickOnOptimizeJPG(){
+            optimizeInJPG.click();
+        }
+        public void clickOnCover(){
+            cover.click();
+        }
+    }
+    public class Preview {
+        public Preview() {
+            PageFactory.initElements(driver, this);
+        }
+
+        @FindBy(xpath = "//*[@id=\"collapseSettings\"]/div[1]/div/div/div/div[1]/label")
+        private WebElement imageSizeRadio;
+        @FindBy(xpath = "//*[@id=\"collapseSettings\"]/div[1]/div/div/div/div[2]/label")
+        private WebElement inscriptionRadio;
+        @FindBy(xpath = "//*[@id=\"thumb_text\"]")
+        private WebElement inscriptionRadioText;
+        @FindBy(xpath = "//*[@id=\"collapseSettings\"]/div[1]/div/div/div/div[3]/label")
+        private WebElement writeFilenameRadio;
+        @FindBy(xpath = "//*[@id=\"collapseSettings\"]/div[1]/div/div/div/div[3]/label")
+        private WebElement withoutInscriptionRadio;
+        @Getter
+        private PreviewSize previewSize = new PreviewSize();
+        public void clickOnImageSizeRadio(){
+            imageSizeRadio.click();
+        }
+        public void clickONInscriptionRadio(){
+            inscriptionRadio.click();
+        }
+        public void clickOnWriteFileNameRadio(){
+            writeFilenameRadio.click();
+        }
+        public void clickOnWithoutInscriptionRation(){
+            withoutInscriptionRadio.click();
+        }
+        public void addInscriptionText(String text){
+            inscriptionRadioText.sendKeys(text);
+        }
+
+        public class PreviewSize {
+            public PreviewSize() {
+                PageFactory.initElements(driver, this);
+            }
+
+            @FindBy(xpath = "//*[@id=\"txt-thumb-size\"]")
+            private WebElement input;
+            @FindBy(xpath = "//*[@id=\"collapseSettings\"]/div[1]/div/div/div/div[5]/div[3]/label")
+            private WebElement byHeight;
+
+            public void changePreviewSize(String newSize){
+                input.sendKeys(newSize);
+            }
+            public void clickOnHeight(){
+                byHeight.click();
+            }
+        }
+    }
 
 }
--- src/test/java/MainPageTest.java
+++ src/test/java/MainPageTest.java
@@ -3,15 +3,23 @@ import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import tpo.lab.ConfProperties;
 import tpo.lab.MainPage;
+import static java.lang.Thread.sleep;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class MainPageTest {
     private static WebDriver webDriver;
+    private final static long MAX_DELAY=10000;
 
     @BeforeAll
     public static void initializeWebDriver() {
@@ -86,6 +94,15 @@ public class MainPageTest {
     }
 
     @Test
+    public void fastPic_shouldRedirectToMainPage() {
+        MainPage mainPage = new MainPage(webDriver);
+        mainPage.clickOnFastPicButton();
+        String actual = webDriver.getCurrentUrl();
+        String expected = ConfProperties.getProperty("mainPage");
+        assertEquals(expected, actual);
+    }
+
+    @Test
     public void androidUpButton_shouldDownloadApp() {
         MainPage mainPage = new MainPage(webDriver);
         File dir = new File(ConfProperties.getProperty("downloadPath"));
@@ -95,8 +112,306 @@ public class MainPageTest {
         assertEquals((before + 1), after);
     }
 
+    @Test
+    public void toolBarButton_shouldDownloadApp() {
+        //todo
+    }
+
+    @Test
+    public void downloadButton_shouldRedirectTiMainPage() {
+        MainPage mainPage = new MainPage(webDriver);
+        mainPage.clickOnDownloadButton();
+        String actual = webDriver.getCurrentUrl();
+        String expected = ConfProperties.getProperty("mainPage");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void downloadByLinkButton_shouldRedirectToDownloadByLinkPage() {
+        MainPage mainPage = new MainPage(webDriver);
+        mainPage.clickOnDownloadByLinkButton();
+        String actual = webDriver.getCurrentUrl();
+        String expected = ConfProperties.getProperty("downloadBuLinkPage");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void myDownloadsButton_shouldRedirectToMyDownloadsPage() {
+        MainPage mainPage = new MainPage(webDriver);
+        mainPage.clickOnMyDownloadsButton();
+        String actual = webDriver.getCurrentUrl();
+        String expected = ConfProperties.getProperty("myDownloadsPage");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void enterButton_shouldRedirectToEnterPage() {
+        MainPage mainPage = new MainPage(webDriver);
+        mainPage.clickOnEnterButton();
+        String actual = webDriver.getCurrentUrl();
+        String expected = ConfProperties.getProperty("enterPage");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void restrictionsButton_shouldOpenRestrictionMessage() {
+        //todo
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "/home/runtic/IdeaProjects/lab3_TPO/src/test/resources/images/test.png",
+            "/home/runtic/IdeaProjects/lab3_TPO/src/test/resources/images/test.gif",
+            "/home/runtic/IdeaProjects/lab3_TPO/src/test/resources/images/test.jpeg",
+            "/home/runtic/IdeaProjects/lab3_TPO/src/test/resources/images/test.bmp",
+            "/home/runtic/IdeaProjects/lab3_TPO/src/test/resources/images/test.webp",
+    })
+    public void downloadCorrectImage(String imagePath) throws InterruptedException {
+        MainPage mainPage = new MainPage(webDriver);
+        mainPage.selectImage(imagePath);
+        mainPage.clickOnBeginDownloadButton();
+        waitDownloading(mainPage);
+        checkIfDownloaded();
+    }
+
+    private static void checkIfDownloaded() {
+        String actual = webDriver.getCurrentUrl();
+        String expected = ConfProperties.getProperty("albumPage");
+        assertTrue(actual.startsWith(expected));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "/home/runtic/IdeaProjects/lab3_TPO/src/test/resources/images/tooBigImage.jpg",
+            "/home/runtic/IdeaProjects/lab3_TPO/src/test/resources/images/wrongExtention.txt"
+    })
+    public void downloadWrongImage(String imagePath) throws InterruptedException {
+        MainPage mainPage = new MainPage(webDriver);
+        mainPage.selectImage(imagePath);
+        mainPage.clickOnBeginDownloadButton();
+        waitDownloading(mainPage);
+        checkIfNotDownloaded();
+    }
+
+    private static void checkIfNotDownloaded() {
+        String actual = webDriver.getCurrentUrl();
+        String expected = ConfProperties.getProperty("albumPage");
+        assertFalse(actual.startsWith(expected));
+    }
+
+    private static void waitDownloading(MainPage mainPage) throws InterruptedException {
+        long start = System.currentTimeMillis();
+        while (!mainPage.isFinishedDownloading()){
+            sleep(1000);
+            if(System.currentTimeMillis()-MAX_DELAY>start){
+                break;
+            }
+        }
+    }
+
+    @Test
+    public void downloadALotOfImages() throws InterruptedException {
+//        MainPage mainPage = new MainPage(webDriver);
+//        for(int i=0;i<1001;i++) {
+//            mainPage.selectImage("/home/runtic/IdeaProjects/lab3_TPO/src/test/resources/images/test.png");
+//        }
+//        mainPage.clickOnBeginDownloadButton();
+//        long start = System.currentTimeMillis();
+//        while (!mainPage.isFinishedDownloading()){
+//            sleep(1000);
+//            if(System.currentTimeMillis()-MAX_DELAY>start){
+//                fail();
+//            }
+//        }
+//        String actual = webDriver.getCurrentUrl();
+//        String expected = ConfProperties.getProperty("albumPage");
+//        assertFalse(actual.startsWith(expected));
+    }
+    @Test
+    public void checkCorrectPreviewRestrictions() throws InterruptedException {
+        MainPage mainPage = new MainPage(webDriver);
+        mainPage.selectImage("/home/runtic/IdeaProjects/lab3_TPO/src/test/resources/images/test.png");
+        mainPage.clickOnSettingsButton();
+        mainPage.getPreview().getPreviewSize().changePreviewSize(String.valueOf(400));
+        mainPage.clickOnBeginDownloadButton();
+        waitDownloading(mainPage);
+        checkIfDownloaded();
+
+    }
+    //error suite work
+    @Test
+    public void checkWrongSizeMoreThan400PreviewRestrictions() throws InterruptedException {
+        MainPage mainPage = new MainPage(webDriver);
+        mainPage.selectImage("/home/runtic/IdeaProjects/lab3_TPO/src/test/resources/images/test.png");
+        mainPage.clickOnSettingsButton();
+        mainPage.getPreview().getPreviewSize().changePreviewSize(String.valueOf(500));
+        mainPage.clickOnBeginDownloadButton();
+        waitDownloading(mainPage);
+        checkIfNotDownloaded();
+    }
+    //error suite work
+    @Test
+    public void checkWrongRestrictionRestrictions() throws InterruptedException {
+        MainPage mainPage = new MainPage(webDriver);
+        mainPage.selectImage("/home/runtic/IdeaProjects/lab3_TPO/src/test/resources/images/test.png");
+        mainPage.clickOnSettingsButton();
+        mainPage.getPreview().getPreviewSize().changePreviewSize("hh");
+        mainPage.clickOnBeginDownloadButton();
+        waitDownloading(mainPage);
+        checkIfNotDownloaded();
+    }
+    @Test
+    public void checkWrongPreviewInscriptionRestrictions() throws InterruptedException {
+        MainPage mainPage = new MainPage(webDriver);
+        mainPage.selectImage("/home/runtic/IdeaProjects/lab3_TPO/src/test/resources/images/test.png");
+        mainPage.clickOnSettingsButton();
+        mainPage.getPreview().clickONInscriptionRadio();
+        mainPage.getPreview().addInscriptionText("kkk");
+        mainPage.clickOnBeginDownloadButton();
+        waitDownloading(mainPage);
+        checkIfDownloaded();
+    }
+    @Test
+    public void checkEmptyPreviewInscriptionRestrictions() throws InterruptedException {
+        MainPage mainPage = new MainPage(webDriver);
+        mainPage.selectImage("/home/runtic/IdeaProjects/lab3_TPO/src/test/resources/images/test.png");
+        mainPage.clickOnSettingsButton();
+        mainPage.getPreview().clickONInscriptionRadio();
+        mainPage.getPreview().addInscriptionText("");
+        mainPage.clickOnBeginDownloadButton();
+        waitDownloading(mainPage);
+        checkIfDownloaded();
+    }
+    @Test
+    public void checkWriteSizeOfImageRationPreview() throws InterruptedException {
+        MainPage mainPage = new MainPage(webDriver);
+        mainPage.selectImage("/home/runtic/IdeaProjects/lab3_TPO/src/test/resources/images/test.png");
+        mainPage.clickOnSettingsButton();
+        mainPage.getPreview().clickOnImageSizeRadio();
+        mainPage.clickOnBeginDownloadButton();
+        waitDownloading(mainPage);
+        checkIfDownloaded();
+    }
+    @Test
+    public void checkWriteFilenameRationPreview() throws InterruptedException {
+        MainPage mainPage = new MainPage(webDriver);
+        mainPage.selectImage("/home/runtic/IdeaProjects/lab3_TPO/src/test/resources/images/test.png");
+        mainPage.clickOnSettingsButton();
+        mainPage.getPreview().clickOnWriteFileNameRadio();
+        mainPage.clickOnBeginDownloadButton();
+        waitDownloading(mainPage);
+        checkIfDownloaded();
+    }
+    @Test
+    public void checkWithoutFilenameRationPreview() throws InterruptedException {
+        MainPage mainPage = new MainPage(webDriver);
+        mainPage.selectImage("/home/runtic/IdeaProjects/lab3_TPO/src/test/resources/images/test.png");
+        mainPage.clickOnSettingsButton();
+        mainPage.getPreview().clickOnWithoutInscriptionRation();
+        mainPage.clickOnBeginDownloadButton();
+        waitDownloading(mainPage);
+        checkIfDownloaded();
+    }
+    @Test
+    public void checkDecreaseTillCorrectImage() throws InterruptedException {
+        MainPage mainPage = new MainPage(webDriver);
+        mainPage.selectImage("/home/runtic/IdeaProjects/lab3_TPO/src/test/resources/images/test.png");
+        mainPage.clickOnSettingsButton();
+        mainPage.getImage().clickOnDeacreaseTillButton();
+        mainPage.getImage().addDeacreaseText("200");
+        mainPage.clickOnBeginDownloadButton();
+        waitDownloading(mainPage);
+        checkIfDownloaded();
+    }
+    //error suite work
+    @Test
+    public void checkDecreaseTillWrongImage() throws InterruptedException {
+        MainPage mainPage = new MainPage(webDriver);
+        mainPage.selectImage("/home/runtic/IdeaProjects/lab3_TPO/src/test/resources/images/test.png");
+        mainPage.clickOnSettingsButton();
+        mainPage.getImage().clickOnDeacreaseTillButton();
+        mainPage.getImage().addDeacreaseText("hh");
+        mainPage.clickOnBeginDownloadButton();
+        waitDownloading(mainPage);
+        checkIfNotDownloaded();
+    }
+    @Test
+    public void checkDecreaseInBrowserImage() throws InterruptedException {
+        MainPage mainPage = new MainPage(webDriver);
+        mainPage.selectImage("/home/runtic/IdeaProjects/lab3_TPO/src/test/resources/images/test.png");
+        mainPage.clickOnSettingsButton();
+        mainPage.getImage().clickOnDecreaseInBrowser();
+        mainPage.clickOnBeginDownloadButton();
+        waitDownloading(mainPage);
+        checkIfDownloaded();
+    }
+    @Test
+    public void checkOptimizeInJpgCorrectImage() throws InterruptedException {
+        MainPage mainPage = new MainPage(webDriver);
+        mainPage.selectImage("/home/runtic/IdeaProjects/lab3_TPO/src/test/resources/images/test.png");
+        mainPage.clickOnSettingsButton();
+        mainPage.getImage().clickOnOptimizeJPG();
+        mainPage.getImage().addOptimiseInJpgText("23");
+        mainPage.clickOnBeginDownloadButton();
+        waitDownloading(mainPage);
+        checkIfDownloaded();
+    }
+    @Test
+    public void checkOptimizeInJpgIncorrectImage() throws InterruptedException {
+        MainPage mainPage = new MainPage(webDriver);
+        mainPage.selectImage("/home/runtic/IdeaProjects/lab3_TPO/src/test/resources/images/test.png");
+        mainPage.clickOnSettingsButton();
+        mainPage.getImage().clickOnOptimizeJPG();
+        mainPage.getImage().addOptimiseInJpgText("hh");
+        mainPage.clickOnBeginDownloadButton();
+        waitDownloading(mainPage);
+        checkIfDownloaded();
+    }
+    @Test
+    public void checkPocterOptionImage() throws InterruptedException {
+        MainPage mainPage = new MainPage(webDriver);
+        mainPage.selectImage("/home/runtic/IdeaProjects/lab3_TPO/src/test/resources/images/test.png");
+        mainPage.clickOnSettingsButton();
+        mainPage.getImage().clickOnCover();
+        mainPage.clickOnBeginDownloadButton();
+        waitDownloading(mainPage);
+        checkIfDownloaded();
+        String xPathToUrl="//*[@id=\"summary-textarea\"]";
+        System.out.println(webDriver.findElement(By.xpath(xPathToUrl)).getAttribute("value"));
+        assertTrue(webDriver.findElement(By.xpath(xPathToUrl)).getAttribute("value").startsWith("https://"));
+    }
+
+    @Test
+    public void checkThatImageAddedToTheWrittenPoster() throws InterruptedException {
+        MainPage mainPage = new MainPage(webDriver);
+        mainPage.selectImage("/home/runtic/IdeaProjects/lab3_TPO/src/test/resources/images/test.png");
+        mainPage.clickOnSettingsButton();
+        mainPage.getImage().clickOnCover();
+        mainPage.clickOnBeginDownloadButton();
+        waitDownloading(mainPage);
+        checkIfDownloaded();
+        String xPathToUrl="//*[@id=\"summary-textarea\"]";
+        assertTrue(webDriver.findElement(By.xpath(xPathToUrl)).getAttribute("value").startsWith("https://"));
+    }
+    @Test
+    public void checkThatImageAddedToTheAlbum() throws InterruptedException {
+        MainPage mainPage = new MainPage(webDriver);
+        mainPage.selectImage("/home/runtic/IdeaProjects/lab3_TPO/src/test/resources/images/test.png");
+        mainPage.clickOnSettingsButton();
+        String expectedAlbumName = "f";
+        mainPage.getDownloadSettings().addAlbumName(expectedAlbumName);
+        waitDownloading(mainPage);
+        checkIfDownloaded();
+        String albumNameXpath="/html/body/div[2]/main/div/div[1]/div/nav/ol/li[2]/span";
+        assertEquals(expectedAlbumName,webDriver.findElement(By.xpath(albumNameXpath)).getText());
+    }
+
+
+
+
+
     @AfterAll
     public static void closeDriver() {
-        webDriver.quit();
+//        webDriver.quit();
     }
 }
